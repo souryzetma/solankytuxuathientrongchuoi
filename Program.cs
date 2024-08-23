31b12a91ea352ba415fef22992fcc16b3391caa6
souryzetma <91234492+souryzetma@users.noreply.github.com>
2024-08-23 19:00:25 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemSoLanXuatHienKiTu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] s = {"a", "h", "h", "b", "w", "c", "c"};
+           Console.WriteLine("Nhap ki tu bat ki co trong mang : ");
+           string x = Console.ReadLine();
+           Console.WriteLine("So lan xuat hien cua ki tu {0} la : ", x);
+           count(s, x);
+        }
+
+         static void count(string[] z, string n)
+        {
+           int dem = 0;
+           for (int i = 0; i < z.Length; i++)
+           {
+               if (z[i] == n)
+                  dem++;
+           }
+           Console.WriteLine(dem);
+        }
+    }
+}
